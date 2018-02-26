eb03c6d37b4399275ea1ecd2dffa17820ffe3a47
logonrm <logonrm@fiap.com.br>
2018-02-26 20:47:42 -0300

    Upload Aula 03

---
+++ aula03/xamarin controles/XF.ControlesBasicos/XF.ControlesBasicos/XF.ControlesBasicos/Configuracao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XF.ControlesBasicos
+{
+    class Configuracao
+    {
+        private bool Aceita;
+
+        public int MyProperty { get; set; }
+    }
+}
